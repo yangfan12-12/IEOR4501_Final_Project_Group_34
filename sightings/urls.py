1955c3b1a3a7a2f8387ee79872d04c174b8d61c1
Cynthia178 <81465498+Cynthia178@users.noreply.github.com>
2021-04-13 16:11:43 +0800

    Update urls.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.base_view),
+    path('map/',views.map),
+    path('sightings/',views.list),
+    path('sightings/stats/',views.stats),
+    path('sightings/add/',views.add),
+    path('sightings/<Unique_Squirrel_Id>/',views.update),
 ]
