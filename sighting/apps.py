7aaf76bd6fbd6a2da0e89c4f9fd42055f446e8df
yangfan12-12 <81471548+yangfan12-12@users.noreply.github.com>
2021-04-13 16:06:32 +0800

    Delete sighting directory

--- sighting/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SightingConfig(AppConfig):
-    name = 'sighting'
--- sighting/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def base_view(request):
-    return render(request,'sighting/base.html', {})
-
-
-
